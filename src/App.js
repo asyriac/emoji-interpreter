8f5ad84866785ce1c1fa62e76a58e9d70cb14fd2
arun_syriac <arundegr8@gmail.com>
2020-12-18 21:37:13 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {dict} from './data';
+
 
 function App() {
+   const [emojiMeaning , setEmojiMeaning] = useState(""); 
+   const emojiList = Object.keys(dict);
+
+   function onChangeHandler(event){
+
+      let meaning = dict[event.target.value];
+
+      if(meaning === undefined){
+         meaning = "Unrecognized emoji. :("
+      }
+
+      setEmojiMeaning(meaning);
+   }
+
+   function onClickHandler(emoji){
+      setEmojiMeaning(dict[emoji]);
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Food Emoji Interpreter</h1>
+       <input className="searchBar" placeholder="Search emoji..." onChange={onChangeHandler}/> 
+       <h1>{emojiMeaning}</h1>
+       <h1> Available emojis</h1>
+       <ul>
+         {
+            emojiList.map(function(emoji,index){
+               return (<li key={index} onClick={()=> onClickHandler(emoji)}>{emoji}</li>)
+            })
+         }
+       </ul>
     </div>
   );
 }
